2aa25bc46475f2ee7b84b859207b4b5ca36a1ee3
ceejeycaracot09 <116365096+ceejeycaracot09@users.noreply.github.com>
2023-05-08 19:28:36 +0800

    Initial Commit

--- src/pages/SignIn/Registration.js
+++ src/pages/SignIn/Registration.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 
 const SignUpForm = () => {
   const [email, setEmail] = useState('');
@@ -61,7 +61,7 @@ const App = () => {
   );
 };
 
-export default App;
+export default App; */
  
 /* import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
@@ -86,3 +86,51 @@ function BasicExample() {
     }]}
   }
 } */
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+
+function SignUp() {
+  const fields = [
+    {label: 'Email', type: 'text', placeholder: 'Email'},
+    {label: 'Username', type: 'text', placeholder: 'Username'},
+    {label: 'Password', type: 'password', placeholder: 'Password'}
+  ];
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="signin-container">
+      <Form>
+        {fields.map((field, index) => {
+          return (
+            <Form.Group className="ab-3" controlId={`signUpForm${index}`} key={index}>
+              <Form.Label>{field.label}</Form.Label>
+              <Form.Control 
+                type={field.type} 
+                placeholder={field.placeholder}
+                onChange={(e) => {
+                  if (field.label === 'Email') {
+                    setEmailAddress(e.target.value);
+                  } else if (field.label === 'Username') {
+                    setUsername(e.target.value);
+                  } else if (field.label === 'Password') {
+                    setPassword(e.target.value);
+                  }
+                }}
+              />
+            </Form.Group>
+          );
+        })}
+        <button type="submit">Sign In</button>
+        <br />
+        <h2> Hi {emailAddress}</h2>
+        <h2> Your username is {username}</h2>
+        <h2> Your password is {password}</h2>
+      </Form>
+    </div>
+  );
+}
+
+export default SignUp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import './home.css';
 
-const Home = () => {
+const home = () => {
   return (
-    <div>
-      <h1>Welcome to My Website!</h1>
-      <p>
-        The concept of quick fix refers to a temporary or short-term 
-        solution to a problem or situation. It is a strategy that 
-        provides an immediate solution to an issue without addressing 
-        the underlying cause. Quick fixes are often used to address 
-        urgent problems, such as technical issues, crises, or 
-        emergencies, or to achieve short-term goals.
-      </p>
-      <br/>
-      <br/>
-      <br/>
-      <p>
-      Do not wait for opportunities, create them." - Roy T. Bennett
-      </p>
+    <div className="home-container">
+      <div className="home-image">
+        <img src="https://tse3.mm.bing.net/th?id=OIP.uncCIs8fiTNIKKlio817cgHaGc&pid=Api&P=0" alt="placeholder" />
+      </div>
+      <div className="home-text">
+        <h1>Welcome to My Website!</h1>
+        <p>
+          The concept of quick fix refers to a temporary or short-term 
+          solution to a problem or situation. It is a strategy that 
+          provides an immediate solution to an issue without addressing 
+          the underlying cause. Quick fixes are often used to address 
+          urgent problems, such as technical issues, crises, or 
+          emergencies, or to achieve short-term goals.
+        </p>
+        <p>
+          Do not wait for opportunities, create them." - Roy T. Bennett
+        </p>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default home;
